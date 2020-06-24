cbbcf85e6dfa4018f82b55272805547334691bc2
ramonsantos <ramonsantos.pe@gmail.com>
2020-06-24 18:53:38 -0300

    Add create basic project files

--- lib/rama/project.rb
+++ lib/rama/project.rb
@@ -3,12 +3,64 @@
 module Rama
   class Project
     def initialize(name, options = [])
-      @name = name
-      @options = options
+      @name      = name
+      @options   = options
+      @directory = "#{Dir.getwd}/#{name}"
     end
 
     def create
       puts "Creating project #{@name}"
+      create_basic_files
+    end
+
+    private
+
+    def create_basic_files
+      create_project_directory
+      create_lib_directory
+      create_sources_directory
+      create_gemfile_file
+      create_gitignore_file
+      create_module_file
+    end
+
+    def create_project_directory
+      FileUtils.mkdir_p(@directory)
+    end
+
+    def create_lib_directory
+      FileUtils.mkdir_p("#{@directory}/lib")
+    end
+
+    def create_sources_directory
+      FileUtils.mkdir_p("#{@directory}/lib/#{@name}")
+    end
+
+    def create_gemfile_file
+      File.write("#{@directory}/Gemfile", gemfile_content)
+    end
+
+    def gemfile_content
+      "# frozen_string_literal: true\n\n" \
+      "source 'https://rubygems.org'\n"
+    end
+
+    def create_gitignore_file
+      FileUtils.cp('resource/_.gitignore', "#{@directory}/.gitignore")
+    end
+
+    def create_module_file
+      File.write("#{@directory}/lib/#{@name}.rb", module_file_content)
+    end
+
+    def module_file_content
+      "# frozen_string_literal: true\n\n" \
+      "module #{module_name}\n" \
+      "end\n"
+    end
+
+    def module_name
+      @name.downcase.split('_').map!(&:capitalize).join
     end
   end
 end
--- test/project_test.rb
+++ test/project_test.rb
@@ -4,9 +4,40 @@ require 'minitest/autorun'
 require 'rama/project'
 
 class ProjectTest < Minitest::Test
-  def test_create_project
-    project = Rama::Project.new('rama')
+  def setup
+    @temp_dir = 'my_project'
+  end
 
-    assert_output(/Creating project rama/) { project.create }
+  def teardown
+    FileUtils.rm_rf(@temp_dir)
+  end
+
+  def test_create_project
+    project = Rama::Project.new('my_project')
+
+    assert_output(/Creating project my_project/) { project.create }
+
+    assert(Dir.exist?('my_project/'))
+    assert(Dir.exist?('my_project/lib'))
+    assert(Dir.exist?('my_project/lib/my_project'))
+    assert(File.exist?('my_project/Gemfile'))
+    assert(File.exist?('my_project/.gitignore'))
+    assert(File.exist?('my_project/lib/my_project.rb'))
+
+    assert_equal(File.read('my_project/Gemfile'), expected_gemfile_content)
+    assert_equal(File.read('my_project/lib/my_project.rb'), expected_module_content)
+  end
+
+  def expected_gemfile_content
+    "# frozen_string_literal: true\n" \
+    "\n" \
+    "source 'https://rubygems.org'\n"
+  end
+
+  def expected_module_content
+    "# frozen_string_literal: true\n" \
+    "\n" \
+    "module MyProject\n" \
+    "end\n"
   end
 end
